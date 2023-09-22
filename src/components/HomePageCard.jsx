9c5c905a39933c2694a4fa7a459ae6215bed1b2c
Owolabi olusegun <52004465+boarnerges@users.noreply.github.com>
2023-09-22 20:27:41 +0100

    CarouselCategory

--- src/components/HomePageCard.jsx
+++ src/components/HomePageCard.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const HomePageCard = ({ title, img, link }) => {
   return (
-    <div className="h-[420ox] bg-white">
-      <div>{title}</div>
-      <div></div>
-      <div></div>
+    <div className="h-[420ox] bg-white z-30 m-3">
+      <div className="text-lg xl:text-xl font-semibold ml-4 mt-4">{title}</div>
+      <div className="h-[300px] m-4">
+        <img className="h-[100%] w-[100%]" src={img} />
+      </div>
+      <div className="text-xs xl:text-sm text-blue-400 ml-4">{link}</div>
     </div>
   );
 };
