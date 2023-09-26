0a2b88fa196d0bffa5c5c096ff275f2b041988e1
Owolabi olusegun <52004465+boarnerges@users.noreply.github.com>
2023-09-26 13:06:25 +0100

    navbar-flex

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,14 +4,30 @@ const NavBar = () => {
   return (
     <header className="min-w-[1000px]">
       <div className="flex bg-amazonclone text-white h-[60px]">
-        <div className="flex">
-          <div>
+        <div className="flex items-center">
+          <img
+            className="h-[35px] w-[100px] m-2"
+            src={"../images/amazon.png"}
+          />
+          <div className="pr-4 pl-4">
             <div className="text-xs xl:text-sm">Deliver to</div>
-            <div className="text-sm xl:text-base">United Kingdom</div>
+            <div className="text-sm xl:text-base font-bold">United Kingdom</div>
           </div>
         </div>
         <div className="flex">Middle</div>
-        <div className="flex">Right</div>
+        <div className="flex items-center">
+          <div className="pr-4 pl-4">
+            <div className="text-xs xl:text-sm">Hello, sign in</div>
+            <div className="text-sm xl:text-base font-bold">
+              Accounts & Lists
+            </div>
+          </div>
+          <div className="pr-4 pl-4">
+            <div className="text-xs xl:text-sm">Returns</div>
+            <div className="text-sm xl:text-base font-bold">& Orders</div>
+          </div>
+          <div className="flex pr-3 pl-3"></div>
+        </div>
       </div>
     </header>
   );
